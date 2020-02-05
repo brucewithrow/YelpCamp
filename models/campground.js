51669d7115e8767001688b6c3c36ce361cb2f72a
brucewithrow <bruce.withrow@gmail.com>
2020-02-05 12:50:48 -0600

    First commit

---
+++ models/campground.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+// schema setup
+const campgroundSchema = new mongoose.Schema({
+	name: String,
+	price: String,
+	image: String,
+	description: String,
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User'
+		},
+		username: String
+	},
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectID,
+			ref: "Comment"
+		}
+	]
+});
+
+module.exports =  Campground = mongoose.model('Campground', campgroundSchema);
---
+++ routes/campgrounds.js
@@ -0,0 +1,92 @@
+const express = require('express'),
+			router  = express.Router(),
+			Campground = require('../models/campground'),
+			middleware = require('../middleware');
+
+// INDEX - display all campgrounds
+router.get('/', (req, res) => {
+	// get all campgrounds from DB
+	Campground.find({}, (err, allCampgrounds) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('campgrounds/index', { campgrounds: allCampgrounds, currentUser: req.user });
+		}
+	});
+});
+
+// CREATE - add a new campground to DB
+router.post('/', middleware.isLoggedIn,(req, res) => {
+	// get data from form and add to campgrouds array
+	var name = req.body.name;
+	var price = req.body.price;
+	var image = req.body.image;
+	var desc = req.body.description;
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	}
+	var newCampground = { name: name, price: price, image: image, description: desc, author: author };
+	// create a new campground and save to DB
+	Campground.create(newCampground, (err, newlyCreated) => {
+		if (err) {
+			console.log(err);
+		} else {
+			// redirect back to campgrounds page
+			console.log(newlyCreated);
+			res.redirect('/campgrounds');
+		}
+	});
+});
+
+// NEW - show form to create new campground
+router.get('/new', middleware.isLoggedIn,(req, res) => {
+	res.render('campgrounds/new');
+});
+
+// SHOW - shows more info about one campground
+router.get('/:id', (req, res) => {
+	// find the campground with provided ID
+	Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(foundCampground);
+			// render show template with that campground
+			res.render('campgrounds/show', { campground: foundCampground });
+		}
+	});
+});
+
+// EDIT CAMPGROUND ROUTE
+router.get('/:id/edit', middleware.checkCampgroundOwnership, (req, res) => {
+	Campground.findById(req.params.id, (err, foundCampground) => {
+		res.render('campgrounds/edit', {campground: foundCampground });
+	});
+});
+
+// UPDATE CAMPGROUND ROUTE
+router.put('/:id', middleware.checkCampgroundOwnership, (req, res) => {
+	// find and update the correct campground
+	Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
+		if(err){
+			res.redirect('/campgrounds');
+		} else {
+			res.redirect('/campgrounds/' + req.params.id);
+		}
+	});
+	// redirect somewhere (show page)
+});
+
+// DESTROY CAMPGROUND ROUTE
+router.delete('/:id', middleware.checkCampgroundOwnership, (req, res) => {
+	Campground.findByIdAndRemove(req.params.id, (err) => {
+		if(err){
+			res.redirect('/campgrounds');
+		} else {
+			res.redirect('/campgrounds');
+		}
+	});
+});
+
+module.exports = router;
